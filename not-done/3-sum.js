8410264a69911ceccef2a69bcf578d40346cd2c7
Haoran Ding <haoran.ding@accuenergy.com>
2025-03-15 16:41:06 -0400

    test

--- not-done/3-sum.js
+++ not-done/3-sum.js
@@ -36,34 +36,26 @@
 // Two pointers approach
 function threeSum(nums) {
     nums.sort((a, b) => a - b);
-    const res = [];
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > 0) break;
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
-
-        let l = i + 1;
-        let r = nums.length - 1;
-        while (l < r) {
-            const sum = nums[i] + nums[l] + nums[r];
-            if (sum > 0) {
-                r--;
-            } else if (sum < 0) {
-                l++;
-            } else {
-                res.push([nums[i], nums[l], nums[r]]);
-                l++;
-                r--;
-                while (l < r && nums[l] === nums[l - 1]) {
-                    l++;
-                }
-                while (l < r && nums[r] === nums[r + 1]) {
-                    r--;
-                }
-            }
+    const result = [];
+    
+    for(let i = 0; i < nums.length; i++) {
+        let low = i+1, high = nums.length-1, sum = 0;
+        
+        while(low < high) {
+            sum = nums[i] + nums[low] + nums[high];
+            
+            if(sum === 0) {
+                result.push([nums[i], nums[low], nums[high]]);
+                while(nums[low+1] === nums[low]) low++;
+                while(nums[high-1] === nums[high]) high--;
+                low++;
+                high--;
+            } else if(sum < 0) low++;
+            else high--;
         }
+        while(nums[i+1] === nums[i]) i++;
     }
-    return res;
+    return result; 
 }
 
 // Hash map approach
