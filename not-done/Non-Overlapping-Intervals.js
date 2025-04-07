257213f3ea74b678d7b674288be105b615e6280b
hding49 <hding49@uwo.ca>
2025-04-07 18:49:48 -0400

    test

--- not-done/Non-Overlapping-Intervals.js
+++ not-done/Non-Overlapping-Intervals.js
@@ -22,3 +22,18 @@
 // -50000 <= starti < endi <= 50000
 
 
+var eraseOverlapIntervals = function(intervals) {
+    let res = 0;
+    intervals.sort((a, b) => a[1] - b[1]);
+    let prev_end = intervals[0][1];
+
+    for (let i = 1; i < intervals.length; i++) {
+        if (prev_end > intervals[i][0]) {
+            res++;
+        } else {
+            prev_end = intervals[i][1];
+        }
+    }
+
+    return res;    
+};
--- not-done/insert-interval.js
+++ not-done/insert-interval.js
@@ -24,3 +24,27 @@
 // 0 <= intervals.length <= 1000
 // newInterval.length == intervals[i].length == 2
 // 0 <= start <= end <= 1000
+
+
+var insert = function(intervals, newInterval) {
+    let merged = [];
+    let i = 0;
+
+    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+        merged.push(intervals[i]);
+        i++;
+    }
+
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])];
+        i++;
+    }
+    merged.push(newInterval);
+
+    while (i < intervals.length) {
+        merged.push(intervals[i]);
+        i++;
+    }
+
+    return merged;
+};
